feeb4e6941d67bb7284f4593b77b168604aa10f2
apple <apple@ind710.local>
2023-07-05 16:49:28 +0530

    initial lloader abd client setup- wip

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,31 @@
-console.log("Its a test log to check if app is running.");
+import { PineconeClient } from "@pinecone-database/pinecone";
+import { PINECONE_API_KEY, PINECONE_ENVIRONMENT } from "./configuration.js";
+import { DirectoryLoader, TextLoader } from "langchain/document_loaders";
+
+// Setup DirectoryLoader to load documents from the ./documents directory
+const loader = new DirectoryLoader("./documents", {
+  ".txt": (path) => new TextLoader(path),
+});
+const docs = await loader.load();
+
+// Setup variables for the filename, question and index settings
+const question = "How to make dixie biscuit?";
+const indexName = "langchain-pinecone-index";
+const vectorDimension = 1536;
+
+// // Initialize Pinecone client API key and environment
+// const client = new PineconeClient();
+// await client.init({
+//   apiKey: PINECONE_API_KEY || "",
+//   environment: PINECONE_ENVIRONMENT || "",
+// })(
+//   // Run the main async function
+//   async () => {
+//     // Check if Pinecone index exists and create if necessary
+//     await createPineconeIndex(client, indexName, vectorDimension);
+//     // Update Pinecone vector store with document embeddings
+//     await updatePinecone(client, indexName, docs);
+//     // Query Pinecone Vector store and GPT model for an answer
+//     await queryPineconeVectorStoreAndQueryLLM(client, indexName, question);
+//   }
+// )();
--- src/configuration.ts
+++ src/configuration.ts
@@ -1,1 +1,8 @@
+import * as dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+export const PINECONE_API_KEY = process.env.PINECONE_API_KEY;
+export const PINECONE_ENVIRONMENT = process.env.PINECONE_ENVIRONMENT;
